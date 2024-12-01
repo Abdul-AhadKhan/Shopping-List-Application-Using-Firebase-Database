aa35a1571f3a4b3fb3c7d3b3870f46d5d6560df6
Abdul-AhadKhan <ahadravi571@gmail.com>
2024-12-01 17:41:24 +0500

    Login, SignUp Design

--- src/main/java/com/example/shoppinglist/Login.java
+++ src/main/java/com/example/shoppinglist/Login.java
@@ -2,7 +2,8 @@ package com.example.shoppinglist;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +11,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Login extends AppCompatActivity {
+
+    TextView signUp, forgotPassword;
+    TextInputEditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        new Handler().postDelayed(new Runnable() {
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                startActivity(new Intent(MainActivity.this, Login.class));
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Register.class));
                 finish();
             }
-        }, 3000);
+        });
+    }
+
+    private void init(){
+        signUp = findViewById(R.id.tvSignUp);
+        forgotPassword = findViewById(R.id.tvForgotPassword);
+        email = findViewById(R.id.LoginEmail);
+        password = findViewById(R.id.LoginPassword);
     }
 }
---
+++ src/main/java/com/example/shoppinglist/Register.java
@@ -0,0 +1,49 @@
+package com.example.shoppinglist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Register extends AppCompatActivity {
+    
+    TextView tvLogin;
+    TextInputEditText name, email, password, confirmPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        
+        init();
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Register.this, Login.class));
+                finish();
+            }
+        });
+    }
+    
+    private void init(){
+        tvLogin = findViewById(R.id.tvLogin);
+        name = findViewById(R.id.RegisterName);
+        password = findViewById(R.id.RegisterPassword);
+        confirmPassword = findViewById(R.id.RegisterConfirmPassword);
+        email = findViewById(R.id.RegisterEmail);
+    }
+}
